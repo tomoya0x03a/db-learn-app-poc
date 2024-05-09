6006746044f494258fda4984b49e0859ccfaa48e
tomoya0x03a <yamada0x03a@gmail.com>
2024-05-09 11:00:18 +0900

    テーブルを作成した。

--- app/react-app/src/App.tsx
+++ app/react-app/src/App.tsx
@@ -16,6 +16,14 @@ import { Height, Padding } from '@mui/icons-material';
 import { styled } from '@mui/system';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import LeaderLine from "leader-line-new";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { TableVirtuoso, TableComponents } from 'react-virtuoso';
 
 const style = {
   p: 0,
@@ -32,9 +40,131 @@ const StyledListItem = styled(ListItem)({
   paddingBottom: '0px',
 });
 
+interface Data {
+  calories: number;
+  carbs: number;
+  dessert: string;
+  fat: number;
+  id: number;
+  protein: number;
+}
+
+interface ColumnData {
+  dataKey: keyof Data;
+  label: string;
+  numeric?: boolean;
+  width: number;
+}
+
+type Sample = [string, number, number, number, number];
+
+const sample: readonly Sample[] = [
+  ['Frozen yoghurt', 159, 6.0, 24, 4.0],
+  ['Ice cream sandwich', 237, 9.0, 37, 4.3],
+  ['Eclair', 262, 16.0, 24, 6.0],
+  ['Cupcake', 305, 3.7, 67, 4.3],
+  ['Gingerbread', 356, 16.0, 49, 3.9],
+];
+
+function createData(
+  id: number,
+  dessert: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+): Data {
+  return { id, dessert, calories, fat, carbs, protein };
+}
+
+const columns: ColumnData[] = [
+  {
+    width: 200,
+    label: 'Dessert',
+    dataKey: 'dessert',
+  },
+  {
+    width: 120,
+    label: 'Calories\u00A0(g)',
+    dataKey: 'calories',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Fat\u00A0(g)',
+    dataKey: 'fat',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Carbs\u00A0(g)',
+    dataKey: 'carbs',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Protein\u00A0(g)',
+    dataKey: 'protein',
+    numeric: true,
+  },
+];
+
+const rows: Data[] = Array.from({ length: 200 }, (_, index) => {
+  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+  return createData(index, ...randomSelection);
+});
+
+const VirtuosoTableComponents: TableComponents<Data> = {
+  Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
+    <TableContainer component={Paper} {...props} ref={ref} />
+  )),
+  Table: (props) => (
+    <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
+  ),
+  TableHead,
+  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
+  TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
+    <TableBody {...props} ref={ref} />
+  )),
+};
+
+function fixedHeaderContent() {
+  return (
+    <TableRow>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          variant="head"
+          align={column.numeric || false ? 'right' : 'left'}
+          style={{ width: column.width }}
+          sx={{
+            backgroundColor: 'background.paper',
+          }}
+        >
+          {column.label}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
+
+function rowContent(_index: number, row: Data) {
+  return (
+    <React.Fragment>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          align={column.numeric || false ? 'right' : 'left'}
+        >
+          {row[column.dataKey]}
+        </TableCell>
+      ))}
+    </React.Fragment>
+  );
+}
 
 export default function ListDividers() {
-  const corporation= useRef<HTMLDivElement>(null);
+  const corporation = useRef<HTMLDivElement>(null);
   const corporateAnniversary = useRef<HTMLDivElement>(null);
   const nationalHoliday = useRef<HTMLDivElement>(null);
   const employee = useRef<HTMLDivElement>(null);
@@ -53,65 +183,68 @@ export default function ListDividers() {
 
   useEffect(() => {
     new LeaderLine(
-        corporation.current,
-        corporateAnniversary.current,
-        leaderSettings,
-    )
-    new LeaderLine(
-        corporation.current,
-        employee.current,
-        leaderSettings,
-    )
-    new LeaderLine(
-        LeaderLine.pointAnchor(corporation.current, {x: '100%', y: '100%'}),
-        LeaderLine.pointAnchor(department.current, {x: '0%', y: '0%'}),
-        leaderSettings,
-    )
-    new LeaderLine(
-        LeaderLine.pointAnchor(department.current, {x: '0%', y: 50}),
-        LeaderLine.pointAnchor(employee.current, {x: '100%', y: 50}),
-        leaderSettings,
-    )
-    new LeaderLine(
-      LeaderLine.pointAnchor(employee.current, {x: '100%', y: 100}),
-        LeaderLine.pointAnchor(department.current, {x: '0%', y: 100}),
-        leaderSettings,
-    )
-    new LeaderLine(
-        employee.current,
-        retirement.current,
-        {...leaderSettings,
-          endPlug: 'behind',
-        }
-    )
-    new LeaderLine(
-      LeaderLine.pointAnchor(employee.current, {x: '100%', y: '100%'}),
-      LeaderLine.pointAnchor(goal.current, {x: '0%', y: '25%'}),
+      corporation.current,
+      corporateAnniversary.current,
       leaderSettings,
     )
     new LeaderLine(
-      LeaderLine.pointAnchor(employee.current, {x: '100%', y: 170}),
-      LeaderLine.pointAnchor(evaluation.current, {x: '0%', y: 170}),
+      corporation.current,
+      employee.current,
       leaderSettings,
     )
     new LeaderLine(
-      LeaderLine.pointAnchor(employee.current, {x: '100%', y: 180}),
-      LeaderLine.pointAnchor(evaluation.current, {x: '0%', y: 180}),
+      LeaderLine.pointAnchor(corporation.current, { x: '100%', y: '100%' }),
+      LeaderLine.pointAnchor(department.current, { x: '0%', y: '0%' }),
       leaderSettings,
     )
     new LeaderLine(
-        goal.current,
-        performance.current,
-        {...leaderSettings,
-          endPlug: 'behind',
-        }
+      LeaderLine.pointAnchor(department.current, { x: '0%', y: 50 }),
+      LeaderLine.pointAnchor(employee.current, { x: '100%', y: 50 }),
+      leaderSettings,
     )
     new LeaderLine(
-        performance.current,
-        evaluation.current,
-        {...leaderSettings,
-          endPlug: 'behind',
-        }
+      LeaderLine.pointAnchor(employee.current, { x: '100%', y: 100 }),
+      LeaderLine.pointAnchor(department.current, { x: '0%', y: 100 }),
+      leaderSettings,
+    )
+    new LeaderLine(
+      employee.current,
+      retirement.current,
+      {
+        ...leaderSettings,
+        endPlug: 'behind',
+      }
+    )
+    new LeaderLine(
+      LeaderLine.pointAnchor(employee.current, { x: '100%', y: '100%' }),
+      LeaderLine.pointAnchor(goal.current, { x: '0%', y: '25%' }),
+      leaderSettings,
+    )
+    new LeaderLine(
+      LeaderLine.pointAnchor(employee.current, { x: '100%', y: 170 }),
+      LeaderLine.pointAnchor(evaluation.current, { x: '0%', y: 170 }),
+      leaderSettings,
+    )
+    new LeaderLine(
+      LeaderLine.pointAnchor(employee.current, { x: '100%', y: 180 }),
+      LeaderLine.pointAnchor(evaluation.current, { x: '0%', y: 180 }),
+      leaderSettings,
+    )
+    new LeaderLine(
+      goal.current,
+      performance.current,
+      {
+        ...leaderSettings,
+        endPlug: 'behind',
+      }
+    )
+    new LeaderLine(
+      performance.current,
+      evaluation.current,
+      {
+        ...leaderSettings,
+        endPlug: 'behind',
+      }
     )
   }, []);
 
@@ -162,7 +295,7 @@ export default function ListDividers() {
               </List>
             </Grid>
             <Grid item xs={3}>
-              <List sx={style} aria-label="mailbox folders"  ref={corporateAnniversary}>
+              <List sx={style} aria-label="mailbox folders" ref={corporateAnniversary}>
                 <StyledListItem>
                   <ListItemText primary="会社記念日" />
                 </StyledListItem>
@@ -350,6 +483,14 @@ export default function ListDividers() {
             <Grid item xs={3}></Grid>
           </Grid>
         </Box>
+        <Paper style={{ height: 400, width: '100%' }}>
+          <TableVirtuoso
+            data={rows}
+            components={VirtuosoTableComponents}
+            fixedHeaderContent={fixedHeaderContent}
+            itemContent={rowContent}
+          />
+        </Paper>
       </Container>
     </div>
   );
